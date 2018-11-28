2c2ae6993bd38da9ed637c2c88447214b388ef67
djones321 <damonjones321@ksu.edu>
2018-11-28 14:09:21 -0600

    Y E E T

--- 501FinalProject/Form1.Designer.cs
+++ 501FinalProject/Form1.Designer.cs
@@ -29,24 +29,38 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(573, 189);
+            this.button1.Location = new System.Drawing.Point(430, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(413, 255);
+            this.button1.Size = new System.Drawing.Size(310, 204);
             this.button1.TabIndex = 0;
             this.button1.Text = "press to die instantly";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.button2.Location = new System.Drawing.Point(155, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 312);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "YEET";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1297, 659);
+            this.ClientSize = new System.Drawing.Size(973, 527);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
