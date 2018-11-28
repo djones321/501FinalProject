6d9ac17df36309768a7c22c377273b0b378fa276
djones321 <damonjones321@ksu.edu>
2018-11-28 14:36:27 -0600

    Hopefully yeet is blue

--- 501FinalProject/Form1.Designer.cs
+++ 501FinalProject/Form1.Designer.cs
@@ -35,10 +35,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(573, 189);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(430, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(413, 255);
+            this.button1.Size = new System.Drawing.Size(310, 204);
             this.button1.TabIndex = 0;
             this.button1.Text = "press to die instantly";
             this.button1.UseVisualStyleBackColor = false;
@@ -46,23 +46,23 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.button2.Location = new System.Drawing.Point(207, 91);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(155, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 390);
+            this.button2.Size = new System.Drawing.Size(68, 312);
             this.button2.TabIndex = 1;
             this.button2.Text = "YEET";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1297, 659);
+            this.ClientSize = new System.Drawing.Size(973, 527);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
