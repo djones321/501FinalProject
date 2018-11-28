83d460de5213723b7c724392f2e6b0cddaa3a84d
Henry Tryban <32393901+TheLizard9110@users.noreply.github.com>
2018-11-28 13:43:09 -0600

    death button

--- 501FinalProject/Form1.cs
+++ 501FinalProject/Form1.cs
@@ -16,5 +16,10 @@ namespace _501FinalProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
